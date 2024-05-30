7aa19924cedd9123208ebb472074d159bdac39f5
RaiKwe24 <143084268+RaiKwe24@users.noreply.github.com>
2024-05-30 23:20:35 +0300

    Update AccountManager.java

--- src/easykanbanapp/AccountManager.java
+++ src/easykanbanapp/AccountManager.java
@@ -35,6 +35,6 @@ public class AccountManager {
     }
 
     private boolean validatePassword(String password) {
-        return password.length() >= 8 && password.matches(".*[A-Z].*") && password.matches(".*\\d.*") && password.matches(".*[!@#$%^&*].*");
+        return password.length() >= 8 && password.matches(".*[A-Z].*") && password.matches(" ") && password.matches(" ");
     }
 }
